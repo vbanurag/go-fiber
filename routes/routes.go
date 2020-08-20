21bf74283cce81b20b65fc91db589c396a31c336
anurag sharma <anurag@msd-my.com>
2020-08-20 17:09:57 +0800

    Update routes files

--- routes/routes.go
+++ routes/routes.go
@@ -2,19 +2,8 @@ package routes
 
 import (
 	"github.com/gofiber/fiber"
-	"github.com/vbanurag/go-fiber/controllers"
 )
 
 func RoutesHandler(api fiber.Router) {
 	users(api)
-}
-
-func users(api fiber.Router) {
-	users := api.Group("/users")
-
-	users.Get("/", controllers.GetAllUsers)
-	// users.Get("/:id", Controller.GetUser)
-	// users.Post("/", Controller.AddUser)
-	// users.Put("/:id", Controller.EditUser)
-	// users.Delete("/:id", Controller.DeleteUser)
 }
---
+++ routes/users.go
@@ -0,0 +1,16 @@
+package routes
+
+import (
+	"github.com/gofiber/fiber"
+	"github.com/vbanurag/go-fiber/controllers"
+)
+
+func users(api fiber.Router) {
+	users := api.Group("/users")
+
+	users.Get("/", controllers.GetAllUsers)
+	// users.Get("/:id", Controller.GetUser)
+	// users.Post("/", Controller.AddUser)
+	// users.Put("/:id", Controller.EditUser)
+	// users.Delete("/:id", Controller.DeleteUser)
+}
